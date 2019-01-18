7d3242cf8ab364de70b2327d5e46af10ad8d7894
Curtis Bloomfield <curtbloom@gmail.com>
2019-01-18 13:23:11 -0500

    Original file

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component {
           <h1>Bloc Jams</h1>
         </header>
         <main>
-
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/Album" component={Album} />
         </main>
       </div>
     );
